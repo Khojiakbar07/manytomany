87236bcb9dc9b28e465f284faadad553ea72f1f0
Hojiakbar Zokirov <hojiakbar312@gmail.com>
2023-07-31 02:06:16 +0500

    CODE RUB:Many to Many Relationship

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,51 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create', function () {
+
+    $post = Post::create(['name'=>'My post PHP']);
+
+    $tag1 = Tag::find(1);
+
+    foreach($post->tags as $tag1){
+        echo $tag1;
+    }
+
+    $video = Video::create(['name'=>'video.mov']);
+
+    $tag2 = Tag::find(2);
+
+    foreach($video->tags as $tag2){
+        echo $tag2;
+    }
+});
+
+Route::get('/read', function () {
+
+    $post = Post::findOrFail(1);
+
+    foreach($post->tags as $tag){
+        echo $tag;
+    }
+});
+
+Route::get('/update', function () {
+
+    $post = Post::findOrFail(1);
+
+    foreach($post->tags as $tag){
+
+        return $tag->whereName('PHP')->update(['name'=>'Update PHP']);
+
+    }
+
 });
